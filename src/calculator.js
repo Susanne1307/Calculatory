973f830153430da0645f00622a80a2e2a4f66690
Susanne1307 <susanneholocher@mail.de>
2020-02-26 14:29:56 +0100

    Add functions clear and result

--- src/calculator.js
+++ src/calculator.js
@@ -1,14 +1,9 @@
-// document.querySelector(".calculator__output").value = "13";
-
-// const calculatorInputs = document.querySelectorAll(".calculator__input");
-// calculatorInputs.forEach(function(calculatorInput) {
-//   calculatorInput.addEventListener("click", function() {
-//     calculatorOutput.value = calculatorInput.innerHTML;
-//   });
-// });
+"use strict";
 
 const calculatorOutput = document.querySelector(".calculator__output");
-calculatorOutput.value = multiply(3, 9);
+const calculatorInputs = document.querySelectorAll(".calculator__input");
+const calculatorResult = document.querySelector(".calculator__result");
+const calculatorClear = document.querySelector(".calculator__clear");
 
 function add(numberOne, numberTwo) {
   return numberOne + numberTwo;
@@ -25,3 +20,19 @@ function divide(numberOne, numberTwo) {
 function multiply(numberOne, numberTwo) {
   return numberOne * numberTwo;
 }
+
+const numberOne = Number(calculatorInputs[5].innerText);
+const numberTwo = Number(calculatorInputs[6].innerText);
+// calculatorOutput.value = subtract(numberOne, numberTwo);
+
+function handleClick() {
+  calculatorOutput.value = subtract(numberOne, numberTwo);
+  console.log("hallo");
+}
+calculatorResult.addEventListener("click", handleClick);
+
+function clear() {
+  calculatorOutput.value = "";
+  console.log("Moin");
+}
+calculatorClear.addEventListener("click", clear);
