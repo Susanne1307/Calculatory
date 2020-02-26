03216af8ce24ed73c76c085e0f32ebd801e1592d
Susanne1307 <susanneholocher@mail.de>
2020-02-26 10:56:24 +0100

    Change layout, add add mathematical operations to calculator.js

--- src/calculator.js
+++ src/calculator.js
@@ -1,1 +1,27 @@
-document.querySelector(".calculator__output").value = "13";
+// document.querySelector(".calculator__output").value = "13";
+
+// const calculatorInputs = document.querySelectorAll(".calculator__input");
+// calculatorInputs.forEach(function(calculatorInput) {
+//   calculatorInput.addEventListener("click", function() {
+//     calculatorOutput.value = calculatorInput.innerHTML;
+//   });
+// });
+
+const calculatorOutput = document.querySelector(".calculator__output");
+calculatorOutput.value = multiply(3, 9);
+
+function add(numberOne, numberTwo) {
+  return numberOne + numberTwo;
+}
+
+function subtract(numberOne, numberTwo) {
+  return numberOne - numberTwo;
+}
+
+function divide(numberOne, numberTwo) {
+  return numberOne / numberTwo;
+}
+
+function multiply(numberOne, numberTwo) {
+  return numberOne * numberTwo;
+}
